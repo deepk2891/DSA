dc35bb1c3d465bd96972c95b1fedc81ee503c611
Deep Kothari <deepk2891@gmail.com>
2024-01-26 22:17:56 +0530

    question-3 count Number Of Digits

---
+++ 03-countNumberOfDigits.js
@@ -0,0 +1,17 @@
+/*
+    Question 3 : count the number of digits of a number
+
+    34252 = 5
+*/
+
+function countNumberOfDigits (num) {
+    num = Math.abs(num)
+    let count = 0
+    do
+    {
+        count++
+        num = Math.floor(num / 10)
+    } while (num > 0)
+    return count
+}
+console.log(countNumberOfDigits(34252));
