8a9a957a6b5cc303674e7060d58e66ce0d342640
Deep Kothari <deepk2891@gmail.com>
2024-01-26 22:17:07 +0530

    question-2 sum Of Digits

---
+++ 02-sumOfDigits.js
@@ -0,0 +1,18 @@
+/*
+    Question 2 : Sum of Digits of a number
+
+    1287 : 1 + 2 + 8 + 7 = 18
+
+    1287 % 10 , reminder = 7 + 8, Q = 128 % 10 = 12
+*/
+
+function sumOfDigits (num) {
+    let sum = 0
+    while (num > 0)
+    {
+        sum += num % 10
+        num = Math.floor(num / 10)
+    }
+    return sum
+}
+console.log(sumOfDigits(1287));
